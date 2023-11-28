e12fc645980288ba06894a8fe9dfabe968fe1de7
ahyun <kaeruruk@naver.com>
2023-11-28 20:07:40 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_camera/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.myapplication_camera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Bitmap bitmap;
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        Button picBtn = findViewById(R.id.pic_btn);
+        picBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityResultPicture.launch(intent);
+            }
+        });
+
+    }//onCreate
+
+    ActivityResultLauncher<Intent> activityResultPicture = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    //결과 OK , 데이터 null 아니면
+                    if( result.getResultCode() == RESULT_OK && result.getData() != null){
+
+                        Bundle extras = result.getData().getExtras();
+
+                        bitmap = (Bitmap) extras.get("data");
+
+                        imageView.setImageBitmap(bitmap);
+                    }
+                }
+            });
+
+}//MainActivity
